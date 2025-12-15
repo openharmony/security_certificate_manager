0fae31584ba1a15864e9de0988d8cbcd6ee28f9b
wxb_code <wangxiaobai@huawei.com>
2025-12-15 19:54:23 +0800

    UkeyANI错误码修改
    Signed-off-by: wxb_code <wangxiaobai@huawei.com>

--- interfaces/kits/common/include/cm_dialog_api_common.h
+++ interfaces/kits/common/include/cm_dialog_api_common.h
@@ -56,6 +56,8 @@ static const std::string SA_INTERNAL_ERROR_MSG = "sa internal error.";
 static const std::string NOT_EXIST_MSG = "the certificate dose not exist.";
 static const std::string NOT_ENTERPRISE_DEVICE_MSG = "The operation does not comply with the device security policy,"
     "such as the device does not allow users to manage the ca certificate of the global user.";
+static const std::string CAPABILITY_NOT_SUPPORTED_MSG = "the capability not supported.";
+static const std::string NO_AVAILABLE_CERTIFICATE_MSG = "no available certificate for authorization"
 
 static const std::unordered_map<int32_t, int32_t> DIALOG_CODE_TO_JS_CODE_MAP = {
     // no permission
@@ -70,6 +72,10 @@ static const std::unordered_map<int32_t, int32_t> DIALOG_CODE_TO_JS_CODE_MAP = {
     { CMR_DIALOG_ERROR_NOT_SUPPORTED, DIALOG_ERROR_NOT_SUPPORTED },
     // The input parameter is invalid
     { CMR_DIALOG_ERROR_PARAM_INVALID, PARAM_ERROR },
+    // The device is not supported
+    { CMR_DIALOG_ERROR_CAPABILITY_NOT_SUPPORTED, DIALOG_ERROR_CAPABILITY_NOT_SUPPORTED },
+    // The device has no available cert
+    { CMR_DIALOG_ERROR_NO_AVAILABLE_CERTIFICATE, DIALOG_ERROR_NO_AVAILABLE_CERTIFICATE },
 
     { CMR_DIALOG_ERROR_PARSE_CERT_FAILED, DIALOG_ERROR_INSTALL_FAILED },
     { CMR_DIALOG_ERROR_ADVANCED_SECURITY, DIALOG_ERROR_INSTALL_FAILED },
@@ -88,6 +94,8 @@ static const std::unordered_map<int32_t, std::string> DIALOG_CODE_TO_MSG_MAP = {
     { CMR_DIALOG_ERROR_NOT_SUPPORTED, DIALOG_NOT_SUPPORTED_MSG },
     { CMR_DIALOG_ERROR_NOT_ENTERPRISE_DEVICE, NOT_ENTERPRISE_DEVICE_MSG },
     { CMR_DIALOG_ERROR_PARAM_INVALID, DIALOG_INVALID_PARAMS_MSG },
+    { CMR_DIALOG_ERROR_CAPABILITY_NOT_SUPPORTED, CAPABILITY_NOT_SUPPORTED_MSG},
+    { CMR_DIALOG_ERROR_NO_AVAILABLE_CERTIFICATE, NO_AVAILABLE_CERTIFICATE_MSG},
 
     { CMR_DIALOG_ERROR_PARSE_CERT_FAILED, DIALOG_OPERATION_FAILED_MSG + PARSE_CERT_FAILED_MSG },
     { CMR_DIALOG_ERROR_ADVANCED_SECURITY, DIALOG_OPERATION_FAILED_MSG + ADVANCED_SECURITY_MSG },
