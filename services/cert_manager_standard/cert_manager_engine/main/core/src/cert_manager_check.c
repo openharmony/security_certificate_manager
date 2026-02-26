c19688ced6f4a9c5083d1ba8f57a1a134cbd1488
Leal <lealtan@163.com>
2026-02-26 12:18:30 +0800

    获取CA证书列表/详情私有API整改
    Signed-off-by: Leal <lealtan@163.com>

--- services/cert_manager_standard/cert_manager_engine/main/core/src/cert_manager_check.c
+++ services/cert_manager_standard/cert_manager_engine/main/core/src/cert_manager_check.c
@@ -84,7 +84,7 @@ int32_t CmServiceGetSystemCertCheck(const uint32_t store, const struct CmBlob *c
         return CMR_ERROR_INVALID_ARGUMENT_URI;
     }
 
-    if (!CmHasCommonPermission()) {
+    if (!CmPermissionCheck(store)) {
         CM_LOG_E("permission check failed");
         return CMR_ERROR_PERMISSION_DENIED;
     }
