c127841e2420fee35b541bffe9f957f0b610c136
lijuce1 <wuzhijie14@huawei.com>
2026-03-03 09:33:40 +0800

    update: 更新文件 cert_manager_uri.c 补充置空
    Signed-off-by: lijuce1 <wuzhijie14@huawei.com>

--- services/cert_manager_standard/cert_manager_engine/main/core/src/cert_manager_uri.c
+++ services/cert_manager_standard/cert_manager_engine/main/core/src/cert_manager_uri.c
@@ -436,6 +436,7 @@ static int32_t DecodePath(struct CMUri *uri, const char *path, uint32_t start, u
             // Security note: Preserve old pointer for memory safety during multi-field parsing.
             if (oldPtr != NULL) {
                 free(oldPtr);
+                oldPtr = NULL;
             }
         } else if (e != NULL) {
             *e = DecodeEnum(path, valueOff, valueLen, values, valueCount);
