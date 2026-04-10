afbd3b5474231d6317b3c9c9be12d24a60a447d7
Leal <lealtan@163.com>
2026-04-10 22:57:42 +0800

    add new test case
    Signed-off-by: Leal <lealtan@163.com>
    Change-Id: Iab174548dca442dfdc6ca3a19f09c8b71f3dd9a6

---
+++ test/fuzz_test/cmimportukeycert_fuzzer/cmimportukeycert_fuzzer.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cmimportukeycert_fuzzer.h"
+
+#include "cert_manager_api.h"
+#include "cm_fuzz_test_common.h"
+#include "cm_test_common.h"
+
+namespace {
+const uint32_t MAX_CERTPURPOSE = 6;
+}
+
+using namespace CmFuzzTest;
+namespace OHOS {
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    uint32_t minSize = sizeof(struct CmBlob) * 2 + sizeof(struct UkeyInfo);
+    uint8_t *myData = nullptr;
+    if (!CopyMyData(data, size, minSize, &myData)) {
+        return false;
+    }
+
+    uint32_t remainSize = static_cast<uint32_t>(size);
+    uint32_t offset = 0;
+
+    struct CmBlob keyUri = { 0, nullptr };
+    if (!GetCmBlobFromBuffer(myData, &remainSize, &offset, &keyUri)) {
+        CmFree(myData);
+        return false;
+    }
+
+    struct CmBlob cert = { 0, nullptr };
+    if (!GetCmBlobFromBuffer(myData, &remainSize, &offset, &cert)) {
+        CmFree(myData);
+        return false;
+    }
+
+    uint32_t certPurpose;
+    if (!GetUintFromBuffer(myData, &remainSize, &offset, &certPurpose)) {
+        CmFree(myData);
+        return false;
+    }
+
+    struct UkeyInfo ukeyInfo = {
+        .certPurpose = static_cast<CmCertificatePurpose>(certPurpose % MAX_CERTPURPOSE)
+    };
+
+    CertmanagerTest::MockHapToken mockHap;
+    (void)CmImportUkeyCert(&keyUri, &cert, &ukeyInfo);
+
+    CmFree(myData);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
