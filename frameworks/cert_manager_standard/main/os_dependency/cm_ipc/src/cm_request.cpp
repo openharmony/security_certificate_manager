f875aac96ff01732b6ffc7b90da1c78d8e845d99
Leal <lealtan@163.com>
2026-07-17 12:27:15 +0800

    修复ipc传输大数据的问题
    Signed-off-by: Leal <lealtan@163.com>

--- frameworks/cert_manager_standard/main/os_dependency/cm_ipc/src/cm_request.cpp
+++ frameworks/cert_manager_standard/main/os_dependency/cm_ipc/src/cm_request.cpp
@@ -121,7 +121,11 @@ int32_t SendRequest(enum CertManagerInterfaceCode type, const struct CmBlob *inB
         data.WriteUint32(outBlob->size);
     }
     data.WriteUint32(inBlob->size);
-    data.WriteBuffer(inBlob->data, static_cast<size_t>(inBlob->size));
+    if (!data.WriteBuffer(inBlob->data, static_cast<size_t>(inBlob->size)) &&
+        !data.WriteRawData(inBlob->data, static_cast<size_t>(inBlob->size))) {
+        CM_LOG_E("WriteBuffer and WriteRawData both failed, size: %u", inBlob->size);
+        return CMR_ERROR_IPC_WRITE_FAIL;
+    }
 
     int error = cmProxy->SendRequest(static_cast<uint32_t>(type), data, reply, option);
     if (error != 0) {
