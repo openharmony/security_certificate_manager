6e116af2700fce892ddcde989c0140c938fcaceb
Leal <tanqingliu@h-partners.com>
2026-07-31 16:28:31 +0800

    refactor(metrics): set ERROR_CODE_COUNT=15 and DIALOG_ERROR_CODE_COUNT=12 explicitly
    Signed-off-by: Leal <tanqingliu@h-partners.com>
    
    AI[0%] Human Fixed[0%] Human[100%] AI Adopted[0%]

--- interfaces/kits/common/include/cm_dialog_api_common.h
+++ interfaces/kits/common/include/cm_dialog_api_common.h
@@ -39,7 +39,7 @@ enum ErrorCode {
     DIALOG_ERROR_NOT_COMPLY_SECURITY_POLICY = 29700005,
     DIALOG_ERROR_PARAMETER_VALIDATION_FAILED = 29700006,
     DIALOG_ERROR_NO_AVAILABLE_CERTIFICATE = 29700007,
-    DIALOG_ERROR_CODE_COUNT,
+    DIALOG_ERROR_CODE_COUNT = 12,
 };
 static const std::string DIALOG_NO_PERMISSION_MSG = "the caller has no permission";
 static const std::string DIALOG_INVALID_PARAMS_MSG = "the input parameters is invalid";
